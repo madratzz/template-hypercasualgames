88f70506b11806946178b908d72b053ed5c8fdd7
Raza Butt <raxashafique@gmail.com>
2020-05-05 23:49:48 +0500

    Added Input Controller Based on LeanTouchControls
    * The Project now has a dependency on LeanTouchControls+
    
    Unity 2019.3.11f1

--- Assets/HyperCasualTemplate/Scripts/Core/Controllers/InputController.cs
+++ Assets/HyperCasualTemplate/Scripts/Core/Controllers/InputController.cs
@@ -1,5 +1,110 @@
-﻿using CustomUtilities;
+﻿using System;
+using CustomUtilities;
+using Lean.Touch;
+using Sirenix.OdinInspector;
+using UnityEngine;
 
-public class InputController : Singleton<InputController>
+namespace HyperCasualTemplate.Scripts.Core.Controllers
 {
+	public class InputController : Singleton<InputController>
+	{
+		//LeanTouch Hooks
+		[SerializeField]
+		private LeanTouch touch;
+		[SerializeField]
+		private LeanFingerSwipe fingerSwipeLeft;
+		[SerializeField]
+		private LeanFingerSwipe fingerSwipeRight;
+		[SerializeField]
+		private LeanFingerTap fingerTap;
+
+
+		//InputManager Events
+		public static Action OnSwipeLeft;
+		public static Action OnSwipeRight;
+		public static Action OnTap;
+
+		[BoxGroup("Settings")]
+		public bool isInputEnabled;
+
+		[BoxGroup("Debugging")]
+		[SerializeField] private bool debugEnabled;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			FindTouchReferences();
+
+			fingerSwipeLeft.OnFinger.AddListener(OnFingerSwipeLeft);
+			fingerSwipeRight.OnFinger.AddListener(OnFingerSwipeRight);
+			fingerTap.OnFinger.AddListener(OnFingerTap);
+		}
+
+		[Button(ButtonSizes.Medium)]
+		private void FindTouchReferences()
+		{
+			touch = FindObjectOfType<LeanTouch>();
+			fingerSwipeLeft = GameObject.Find("SwipeLeft").GetComponent<LeanFingerSwipe>();
+			fingerSwipeRight = GameObject.Find("SwipeRight").GetComponent<LeanFingerSwipe>();
+			fingerTap = GameObject.Find("LeanTouch").GetComponent<LeanFingerTap>();
+		}
+
+		private void OnFingerSwipeLeft(LeanFinger leanFinger)
+		{
+			if (isInputEnabled)
+			{
+				if (debugEnabled)
+					Debug.Log("Input: Swipe Left");
+				OnSwipeLeft?.Invoke();
+			}
+			else
+			{
+				if (debugEnabled)
+				{
+					Debug.Log("Input not Enabled");
+				}
+			}
+		}
+
+		private void OnFingerSwipeRight(LeanFinger leanFinger)
+		{
+			if (isInputEnabled)
+			{
+				if (debugEnabled)
+					Debug.Log("Input: Swipe Right");
+				OnSwipeRight?.Invoke();
+			}
+			else
+			{
+				if (debugEnabled)
+				{
+					Debug.Log("Input not Enabled");
+				}
+			}
+		}
+
+		private void OnFingerTap(LeanFinger leanFinger)
+		{
+			if (isInputEnabled)
+			{
+				if (debugEnabled)
+					Debug.Log("Input: Tap");
+				OnTap?.Invoke();
+			}
+			else
+			{
+				if (debugEnabled)
+				{
+					Debug.Log("Input not Enabled");
+				}
+			}
+		}
+
+		private void OnDestroy()
+		{
+			fingerSwipeLeft.OnFinger.RemoveListener(OnFingerSwipeLeft);
+			fingerSwipeRight.OnFinger.RemoveListener(OnFingerSwipeRight);
+			fingerTap.OnFinger.RemoveListener(OnFingerTap);
+		}
+	}
 }
